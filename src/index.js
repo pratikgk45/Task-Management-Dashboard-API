9801616d6f66c20e1d5bf579ae852a2014e19c1e
Pratik Kale <kalepg45@gmail.com>
2021-09-10 22:22:30 +0530

    remote host added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors({
-    origin: ['http://localhost:8000']
+    origin: ['http://localhost:8000', 'https://org-task-manager-app.herokuapp.com']
 }));
 
 app.use(express.json());
